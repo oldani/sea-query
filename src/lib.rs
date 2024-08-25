39ba445c368c224f08b814dc360fc050ade5ab29
Ordanis Sanchez <ordanisanchez@gmail.com>
2024-08-25 13:42:31 -0400

    Export queries

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,12 @@ mod utils;
 fn sea_query(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<query::Query>()?;
     m.add_class::<query::Condition>()?;
+    m.add_class::<query::OrderBy>()?;
+    m.add_class::<query::NullsOrder>()?;
+    m.add_class::<query::SelectStatement>()?;
+    m.add_class::<query::InsertStatement>()?;
+    m.add_class::<query::UpdateStatement>()?;
+    m.add_class::<query::DeleteStatement>()?;
     m.add_class::<expr::SimpleExpr>()?;
     m.add_class::<expr::Expr>()?;
     m.add_class::<utils::DBEngine>()?;
